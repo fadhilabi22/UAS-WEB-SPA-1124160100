45aa4cc687e0f3f76cea1bbe0b55d2f6c86c19dc
unknown <fadielabi4567@gmail.com>
2025-06-20 18:41:35 +0700

    membuat  mapping nama metode pembayaran

--- js/script.js
+++ js/script.js
@@ -11,3 +11,10 @@
             'credit': 'bg-orange-100 text-orange-800 dark:bg-orange-900 dark:text-orange-200',
             'cash': 'bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-200'
         };
+         // Mapping nama metode pembayaran
+        const paymentMethodNames = {
+            'transfer': 'Transfer Bank',
+            'ewallet': 'E-Wallet',
+            'credit': 'Kartu Kredit',
+            'cash': 'Bayar Tunai'
+        };
