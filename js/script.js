4727ecc1bc1a0959092c14848e71abecfc791f7a
unknown <fadielabi4567@gmail.com>
2025-06-20 18:48:45 +0700

    format waktu

--- js/script.js
+++ js/script.js
@@ -89,3 +89,14 @@
                 minimumFractionDigits: 0
             }).format(amount);
         }
+         // Format waktu
+        function getCurrentTime() {
+            const now = new Date();
+            return now.toLocaleString('id-ID', {
+                year: 'numeric',
+                month: '2-digit',
+                day: '2-digit',
+                hour: '2-digit',
+                minute: '2-digit'
+            });
+        }
